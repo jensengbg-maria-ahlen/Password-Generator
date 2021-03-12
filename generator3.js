4da717281ede3a57fbf4284ee87c85909cf73d7c
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2021-03-12 11:39:25 +0100

    created with api

--- generator3.js
+++ generator3.js
@@ -4,11 +4,34 @@ Gör en sökning med hjälp av Have I Been Pwned Password API om det genererade 
 
 API: https://haveibeenpwned.com/API/v3#SearchingPwnedPasswordsByRange
 
-Obs! API:et returnerar inte JSON, utan text.
-
-För att kunna använda API:et behöver man hasha det genererade lösenordet med SHA-1. Använd npm-modulen js-sha1. För att kunna använda npm-moduler i vanilla JS så behöver du kunna bundla ditt JavaScript, du kan använda browserify till det.
-
-Browersify: http://browserify.org/
-
 SHA-1: https://www.npmjs.com/package/js-sha1
 */
+
+const sha1 = require('js-sha1');
+const axios = require('axios');
+
+const API_URL = 'https://api.pwnedpasswords.com/range';
+const PASSWORD = 'qwerty';
+
+( async () => {
+    let hash = sha1(PASSWORD);
+
+    let prefix = hash.substr(0,5);
+    let suffix = hash.substr(5, hash.length);
+    console.log('suffix: ', suffix)
+
+    let resp = await axios(`${API_URL}/${prefix}`);
+    let hashes = resp.data.split('\r\n');
+
+    let msg = 'All good, nice password!'
+    
+    for(let i = 0; i < hashes.length; i++) {
+        let h = hashes[i].split(':');
+
+        if(h[0] === suffix.toUpperCase()) {
+            msg = `Oh no, your password has been used in ${h[1]} times. Don't use it!`
+            break;
+        } 
+    }
+    console.log(msg)
+})()
